bde050da8cb95a15b6020b8bb76385664cccd4df
Prashant <134909999+PrAshAnt2416@users.noreply.github.com>
2024-12-16 13:46:43 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,12 @@
 import { io } from 'socket.io-client';
 
+const SOCKET_TIMEOUT = 10000; // Define a constant for the timeout value
+
 export const initSocket = async () => {
     const options = {
         'force new connection': true,
-        reconnectionAttempt: 'Infinity',
-        timeout: 10000,
+        reconnectionAttempts: Infinity, // Correct property name and value type
+        timeout: SOCKET_TIMEOUT, // Use the constant
         transports: ['websocket'],
     };
     return io(process.env.REACT_APP_BACKEND_URL, options);
